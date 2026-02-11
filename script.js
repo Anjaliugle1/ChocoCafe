a1ae4f5eaf6c74bf056d2a308d5db6db28a2b031
Anjali <ugleanjali@gmail.com>
2026-02-11 11:25:41 +0530

    Updated footer and map button

--- script.js
+++ script.js
@@ -7,18 +7,39 @@ function addToCart(item, price) {
 
   document.getElementById("cartCount").innerText = cart.length;
   updateCart();
+  showMessage();
+}
+
+function showMessage() {
+  const msg = document.getElementById("message");
+  msg.style.display = "block";
+
+  setTimeout(() => {
+    msg.style.display = "none";
+  }, 1500); // hide after 1.5 seconds
 }
 
 function updateCart() {
   const cartItems = document.getElementById("cartItems");
   cartItems.innerHTML = "";
 
-  cart.forEach((c) => {
+  cart.forEach((c, index) => {
     const li = document.createElement("li");
-    li.className = "list-group-item d-flex justify-content-between";
-    li.innerHTML = `<span>${c.item}</span><span>₹${c.price}</span>`;
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = `
+      <span>${c.item} - ₹${c.price}</span>
+      <button class="btn btn-sm btn-danger" onclick="removeItem(${index})">X</button>
+    `;
     cartItems.appendChild(li);
   });
 
   document.getElementById("totalPrice").innerText = total;
+}
+
+function removeItem(index) {
+  total -= cart[index].price;
+  cart.splice(index, 1);
+
+  document.getElementById("cartCount").innerText = cart.length;
+  updateCart();
 }
